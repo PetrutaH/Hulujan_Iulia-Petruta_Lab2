1c51bca06d963a657fa607e09418b70f94a88679
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2024-10-27 21:59:23 +0200

    lab3-fara ultima cerinta

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Hulujan_Iulia_Petruta_Lab2.Data;
 using Hulujan_Iulia_Petruta_Lab2.Models;
+using System.Net;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hulujan_Iulia_Petruta_Lab2.Pages.Books
 {
@@ -21,22 +25,52 @@ namespace Hulujan_Iulia_Petruta_Lab2.Pages.Books
 
         public Book Book { get; set; } = default!;
 
+        [Display(Name = "Author")]
+        public string AuthorFullName { get; set; }
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+
             if (id == null)
             {
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
-            if (book == null)
+            Book = await _context.Book
+               .Include(b => b.Publisher)
+                .Include(b => b.Author)
+                .Include(b => b.BookCategories)
+                .ThenInclude(b => b.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+
+            if (Book == null)
             {
                 return NotFound();
             }
-            else
-            {
-                Book = book;
-            }
+            
+            var authorFullName = Book.Author.FirstName + " " + Book.Author.LastName;
+
+            this.AuthorFullName = authorFullName;
+
+            //Book.Categories = _context.Category.Select(x +> s.Category).Where(BookID = id);
+
+            //var bookCategories 
+
+            //this.Book.
+
+            //BookD.Books = Book;
+
+            //if (id != null)
+            //{
+            //    BookID = id.Vale;
+            //    Book book = BookD.Books
+            //        .Where(i => i.ID == id.Value).Single();
+            //    BookD.Categories = book.BookCategories.Select(s => s.Category);
+            //}
+
+
             return Page();
         }
     }
