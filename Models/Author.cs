fb212942a7bece7cc39fa752218c6662c0e2883d
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2024-10-28 18:40:49 +0200

    lab4-realizat pana la punctul 13

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,4 +1,6 @@
-﻿namespace Hulujan_Iulia_Petruta_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hulujan_Iulia_Petruta_Lab2.Models
 {
     public class Author
     {
@@ -6,6 +8,16 @@
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
 
         public ICollection<Book>? Books { get; set; }
     }
